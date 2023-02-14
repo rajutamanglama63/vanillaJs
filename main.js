755ba3b35486303b01a90543dec9ebe72e00669b
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-02-14 14:34:25 +0545

    chore: dom manipulation

---
+++ main.js
@@ -0,0 +1,32 @@
+// const heading = document.querySelector("#main-heading")
+// console.log(heading)
+
+// const allTags = document.getElementsByTagName("h1")
+// console.log(allTags)
+
+// const parent = document.querySelector(".parent")
+// console.dir(parent)
+// const child = parent.childNodes
+// console.log(child)
+
+// const anotherParent = document.querySelector(".another-parent")
+// const children = anotherParent.childNodes
+// console.log(children)
+
+// const child = document.querySelector("#welcome")
+// console.dir(child)
+// const parent = child.parentNode
+// console.log(parent)
+
+// const contact = document.querySelector("#contact")
+// console.dir(contact)
+// const previousSibling = contact.previousElementSibling
+// console.log(previousSibling)
+
+// const home = document.querySelector("#home")
+// console.dir(contact)
+// const nextSibling = home.nextElementSibling
+// console.log(nextSibling)
+
+const welcome = document.querySelector("#welcome")
+welcome.innerHTML = "Hello world"
